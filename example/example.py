22cdea817d0c343bfac83f6877a53ab1c67c3caa
Bennett Murphy <murphy214@live.marshall.edu>
2016-02-20 17:15:47 -0500

    Update example.py

--- example/example.py
+++ example/example.py
@@ -1,7 +1,7 @@
 import berrl as bl
 
 # please, if possible, don't abuse this key its not difficult to get your own
-apikey='pk.eyJ1IjoibXVycGh5MjE0IiwiYSI6ImNpam5kb3puZzAwZ2l0aG01ZW1uMTRjbnoifQ.5Znb4MArp7v3Wwrn6WFE6A'
+apikey='your api key'
 
 a=bl.make_points('sharks.csv')
 bl.parselist(a,'sharks.geojson') # simply writes a list of lines to file name location
